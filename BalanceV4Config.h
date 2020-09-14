cede12a5ef69084cd0db45ac0b501382014c8774
AML225 <37520295+AML225@users.noreply.github.com>
2020-09-14 10:20:17 -0700

    Update BalanceV4Config.h

--- BalanceV4Config.h
+++ BalanceV4Config.h
@@ -44,8 +44,8 @@ BladeConfig blades[] = {
 CONFIGARRAY(presets) },
 
  { NO_BLADE, 
-	WS281XBladePtr<5, blade2Pin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(), 			//SCW PCB NPXL			(n=5)
-	WS281XBladePtr<2, bladePin, Color8::GRB, PowerPINS<bladePowerPin1> >(),						//Crystal Chamber 		(n=2)
+	SubBlade(0, 4, WS281XBladePtr<269, blade2Pin, Color8::GRB, PowerPINS<bladePowerPin2, bladePowerPin3> >(), 	//SCW PCB NPXL			(n=5)
+    	SubBlade(5, 268, NULL),												//KR Pixel Stick		(n=264)
 	SimpleBladePtr<CreeXPE2White, NoLED, NoLED, bladePowerPin4, -1, -1, -1>0, 					//Red Contol Box 		(n=1)
 	SimpleBladePtr<CreeXPE2White, NoLED, NoLED, bladePowerPin5, -1, -1, -1>0, 					//Green Control Box LED		(n=1)
 CONFIGARRAY(presets2) },
